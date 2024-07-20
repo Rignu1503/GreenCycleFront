854017d47736463a1d623863f20679e122045975
Rignu1503 <rigoberto.miranda1503@gmail.com>
2024-07-20 18:42:25 -0500

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,6 @@
 module.exports = {
   theme: {
     extend: {
-<<<<<<< HEAD
-      keyframes: {
-        slideUp: {
-          '0%': {
-            transform: 'translateY(100%)',
-            opacity: '0',
-          },
-          '100%': {
-            transform: 'translateY(0)',
-            opacity: '1',
-          },
-        },
-      },
-      animation: {
-        slideUp: 'slideUp 5s ease-out', 
-      },
-    },
-  },
-  plugins: [],
-};
-
-
-=======
     },
   },
   plugins: [
@@ -45,4 +22,3 @@ module.exports = {
     }
   ],
 };
->>>>>>> be3bc3a8fe14f46ab621c622c5e8260fa96748c5
